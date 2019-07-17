989419d2ed934fb6194c1f687a5a28be56a4ab4f
gisellehargrove <gisellehargrove@gmail.com>
2019-07-17 15:17:25 -0400

    displaying iframe or image depending on response

--- src/components/APOD/apod.js
+++ src/components/APOD/apod.js
@@ -3,14 +3,16 @@ import React, { useState,useEffect } from 'react';
 function APOD(props) {
   const [media, setMedia] = useState(<img src={props.data.url}></img>)
   const iframe = <iframe src={props.data.url}></iframe>;
+  const image = <img src={props.data.url}></img>;
 
   useEffect(() => {
     if(props.data.media_type === 'video'){
       setMedia(iframe);
+    } else {
+      setMedia(image);
     }
   }, [props.data.media_type]);
 
-  const image = <img src={props.data.url}></img>;
 
   if (!props.data.url) return <h3>Loading...</h3>;
   return (
