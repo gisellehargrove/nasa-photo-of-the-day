3074b28237c276bf6404885a1984c15ce4a43df4
gisellehargrove <gisellehargrove@gmail.com>
2019-07-17 14:24:34 -0400

    added axios dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <p>
