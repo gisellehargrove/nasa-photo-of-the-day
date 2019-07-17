9c1e8e28f3344005b6d286ba1c40fc5d2719903f
gisellehargrove <gisellehargrove@gmail.com>
2019-07-17 14:48:27 -0400

    create nasa component and display info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
+import Nasa from './components/Nasa/nasa.js';
 
 function App() {
+
+  const [data, setData] = useState({});
+
   useEffect(() => {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then((response) => {
       console.log(response.data);
+      setData(response.data);
     });
   }, []);
 
@@ -14,7 +19,9 @@ function App() {
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
+
       </p>
+      <Nasa data={data} />
     </div>
   );
 }
---
+++ src/components/Nasa/nasa.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function Nasa(props) {
+
+  console.log(props)
+  return (
+    <div className="nasa-container">
+      Nasa Component
+      <div className="title-container">
+        <h1 className="title">{props.data.title}</h1>
+      </div>
+      <div className="img-container">
+        <img src={props.data.url}></img>
+        <iframe src={props.data.url}></iframe>
+      </div>
+      <div className="explanation-container">
+        <p className="explanation">
+          {props.data.explanation}
+        </p>
+      </div>
+      <div className="date-container">
+        <span className="date">{props.data.date}</span>
+      </div>
+
+    </div>
+  )
+};
+
+export default Nasa;
